70692fd77cb8b730c2f7312549ace0b569b29938
Delmer Reed <delmer814@yahoo.com>
2014-07-24 11:08:10 -0700

    changes to setup

--- week_03_intro_web_applications/day_04_db_models_and_more_sql/dawn_joins/joins_lesson/friends_example/setup.sql
+++ week_03_intro_web_applications/day_04_db_models_and_more_sql/dawn_joins/joins_lesson/friends_example/setup.sql
@@ -21,12 +21,32 @@ INSERT INTO biking_friends (name) VALUES ('jen doe');
 INSERT INTO biking_friends (name) VALUES ('jesse doe');
 
 
-INSERT INTO running_friends (name) VALUES ('john doe');
-INSERT INTO running_friends (name) VALUES ('jane doe');
-INSERT INTO running_friends (name) VALUES ('ruby doe');
 INSERT INTO running_friends (name) VALUES ('max doe');
 INSERT INTO running_friends (name) VALUES ('jen doe');
+INSERT INTO running_friends (name) VALUES ('john doe');
+INSERT INTO running_friends (name) VALUES ('jane doe');
 INSERT INTO running_friends (name) VALUES ('jesse doe');
+INSERT INTO running_friends (name) VALUES ('ruby doe');
+
+
+
+
+SELECT r.id AS runner_id_for_a_biker
+    FROM biking_friends AS b
+    LEFT JOIN running_friends AS r
+    ON b.name=r.name;
+
+SELECT biker.id, biker.name AS biker_name, runner.id, runner.name runner_name 
+FROM biking_friends AS biker
+FULL OUTER JOIN running_friends AS runner   
+ON biker.name=runner.name;
+
+
+ SELECT biker.id, biker.name AS biker_name, runner.name            FROM biking_friends AS biker                                                        LEFT JOIN running_friends AS runner                                                 ON biker.name=runner.name                                                           WHERE runner IS NULL;
+
+
+
+
 
 
 
