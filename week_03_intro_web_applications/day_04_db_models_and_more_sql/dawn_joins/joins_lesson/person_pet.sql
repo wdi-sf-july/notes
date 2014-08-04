02c2389ad5cadf5c5246a91a3faed50e2d3f3263
Delmer Reed <delmer814@yahoo.com>
2014-08-04 09:56:49 -0700

    untracked notes

---
+++ week_03_intro_web_applications/day_04_db_models_and_more_sql/dawn_joins/joins_lesson/person_pet.sql
@@ -0,0 +1,49 @@
+DROP DATABASE IF EXISTS person_pet_app;
+CREATE DATABASE person_pet_app;
+
+\c person_pet_app
+
+CREATE TABLE person (
+      id serial primary key,
+      name text,
+      age integer
+    );
+
+CREATE TABLE pet (
+  id serial primary key,
+  name text,
+  age integer,
+  breed text,
+  person_id integer
+);
+
+  INSERT INTO person ( name, age)
+        VALUES ('Zed', 37);
+
+   INSERT INTO person ( name, age)
+        VALUES ('Bobby', 7);
+
+  INSERT INTO pet (name, breed, age, person_id)
+        VALUES ( 'Fluffy', 'Unicorn', 1000, 1);
+
+  INSERT INTO pet (name, breed, age, person_id)
+        VALUES ('Rocko', 'Dog', 4, 2);
+
+ INSERT INTO pet (name, breed, age, person_id)
+       VALUES ('Gigantor', 'Robot', 25, 1);
+
+INSERT INTO pet (name, breed, age, person_id)
+       VALUES ('Goldy', 'Fish', 1, 2);
+
+
+\d+ person
+
+\d+ pet
+
+
+
+
+
+
+
+
---
+++ week_03_intro_web_applications/day_04_db_models_and_more_sql/dusk_db_modeling/node_pg_example/fibannoci.js
@@ -0,0 +1,1 @@
+fibannoci.js
